7be2fc606ddcda89a76b852f9829d27e6833ec9f
Asmaa Saif Zidan <159890229+AsmaaSaifZidan@users.noreply.github.com>
2024-02-13 12:18:32 +0200

    Add files via upload

---
+++ src/paintapplication/Main.java
@@ -0,0 +1,12 @@
+ 
+package paintapplication;
+
+public class Main 
+{
+    public static PaintApplication paint; 
+    
+    public static void main(String[] args) 
+    {
+        paint = new PaintApplication();
+    }
+} 
